f12421d8e16d678a18a68b8d65d3d156f7430a4b
nxa127 <nxa127@gmail.com>
2023-09-18 15:28:54 +0700

    Add userheat script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import React from "react";
 import Head from "next/head";
+import Script from "next/script";
 
 import ClientLayout from "@/layout/ClientLayout";
 
@@ -12,7 +13,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>Heatmap demo</title>
       </Head>
-
+      <Script
+        id="uhmBUOhjWP"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(add, cla){window['UserHeatTag']=cla;window[cla]=window[cla]||function(){(window[cla].q=window[cla].q||[]).push(arguments)},window[cla].l=1*new Date();var ul=document.createElement('script');var tag = document.getElementsByTagName('script')[0];ul.async=1;ul.src=add;tag.parentNode.insertBefore(ul,tag);})('//uh.nakanohito.jp/uhj2/uh.js', '_uhtracker');_uhtracker({id:'uhmBUOhjWP'});
+            `,
+        }}
+      />
       <noscript>
         <iframe
           src="https://www.googletagmanager.com/ns.html?id=GTM-NZRG83WX"
